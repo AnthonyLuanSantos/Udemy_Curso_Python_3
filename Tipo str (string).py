c1f4f30a12b1cae607739a8249bdf1214116f7b1
Anthony Luan <anthonyluan_ss@outlook.com>
2026-01-16 10:11:08 -0300

    Add files via upload

---
+++ Tipo str (string).py
@@ -0,0 +1,22 @@
+"""
+DocString
+Python = Linguagem de programação
+Tipo de tipagem = Dinâmica / Forte
+str -> string -> texto
+Strings são textos que estão dentro de aspas
+"""
+print(1234)
+
+# Aspas simples
+print('Anthony Luan')
+print(1, 'Anthony "Luan"')
+
+# Aspas duplas
+print("Anthony Luan")
+print(2, "Anthony 'Luan'")
+
+# Escape
+print("Anthony \"Luan\"")
+
+# r
+print(r"Anthony \"Luan\"")
